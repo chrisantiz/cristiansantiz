c535594baa858f082552009cbeb8b775186bcc5f
Cristian Santiz <crisantizan@gmail.com>
2020-02-20 23:16:30 -0500

    feat: add temporal logo

--- src/components/toolbar/Toolbar.tsx
+++ src/components/toolbar/Toolbar.tsx
@@ -3,8 +3,22 @@ import './toolbar.scss';
 import { DrawerToggleButton } from '../side-drawer/DrawerToggleButton';
 import { ToolbarItems } from './ToolbarItems';
 import { RootContext } from '../../context/root/root.context';
+import { Link, useStaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
 
 export const Toolbar = ({ drawerHandleClick, changeColorOnScroll }: any) => {
+  const image = useStaticQuery(graphql`
+    {
+      file(relativePath: { eq: "logo.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 100) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `);
+
   const { getState } = useContext(RootContext);
   const transparent = getState(state => state.toolbarTransparent);
 
@@ -27,7 +41,9 @@ export const Toolbar = ({ drawerHandleClick, changeColorOnScroll }: any) => {
           <DrawerToggleButton onClick={drawerHandleClick} />
         </div>
         <div className="toolbar-logo">
-          <a href="#">LOGO</a>
+          <Link to="/">
+            <Img fluid={image.file.childImageSharp.fluid} />
+          </Link>
         </div>
         <div className="spacer"></div>
         <ToolbarItems className={`toolbar-navigation-items ${itemsClasess}`} />
--- src/components/toolbar/ToolbarItems.tsx
+++ src/components/toolbar/ToolbarItems.tsx
@@ -5,17 +5,26 @@ export const ToolbarItems = ({ className }: any) => (
   <div className={className}>
     <ul>
       <li>
-        <Link to="/" activeClassName="active">Inicio</Link>
+        <Link to="/" activeClassName="active">
+          Inicio
+        </Link>
       </li>
       <li>
-        <Link to="/sobre-mi" activeClassName="active">Sobre mí</Link>
+        <Link to="/sobre-mi" activeClassName="active" partiallyActive>
+          Sobre mí
+        </Link>
       </li>
       <li>
-        <Link to="/proyectos" activeClassName="active">Proyectos</Link>
+        <Link to="/proyectos" activeClassName="active" partiallyActive>
+          Proyectos
+        </Link>
       </li>
       <li>
-        <Link to="/contacto" activeClassName="active">Contacto</Link>
+        <Link to="/contacto" activeClassName="active" partiallyActive>
+          Contacto
+        </Link>
       </li>
     </ul>
   </div>
+  // Iconos diseñados por <a href="https://www.flaticon.es/autores/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.es/" title="Flaticon"> www.flaticon.es</a>
 );
