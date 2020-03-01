3e384bd2f1c2c73afe3e29386fe2ce3205bcd1e6
Cristian Santiz <crisantizan@gmail.com>
2020-02-29 21:53:58 -0500

    feat: render posts data

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,22 +1,53 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { Link, graphql } from 'gatsby';
 import { SEO } from '@components/Seo';
 import { PageContainer } from '@components/PageContainer';
 import { useLanguage } from '@libs/hooks/use-language';
 
+interface BlogItem {
+  id: string;
+  title: string;
+  date: string;
+}
+
 const BlogPage = (props: any) => {
-  const { lang } = useLanguage();
+  // const { lang } = useLanguage();
+
+  const blogs: BlogItem[] = props.data.allMdx.edges.map((edge: any) => {
+    return { id: edge.node.id, ...edge.node.frontmatter };
+  });
+
+  // console.log(blogs);
   return (
     <>
       <SEO title="Blog" />
       <PageContainer>
-        <p>Esta es la página de blog</p>
-        <Link to="/en">English</Link>
-        <pre>{lang.labels.githubBtn}</pre>
-        <pre>{lang.pages.home.text}</pre>
+        <ul>
+          {blogs.map(blog => (
+            <li key={blog.id}>
+              <h3>{blog.title}</h3>
+              <p>{blog.date}</p>
+            </li>
+          ))}
+        </ul>
       </PageContainer>
     </>
   );
 };
+
+export const query = graphql`
+  query MyQuery($locale: String!) {
+    allMdx(filter: { fields: { locale: { eq: $locale } } }) {
+      edges {
+        node {
+          frontmatter {
+            title
+            date(fromNow: true, locale: $locale)
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default BlogPage;
