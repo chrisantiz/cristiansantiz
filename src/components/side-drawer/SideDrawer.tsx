3065edcfa0a89423ebeacb6f47ce1f6d45793143
Cristian Santiz <crisantizan@gmail.com>
2020-03-03 17:02:24 -0500

    style: add photo

--- src/components/side-drawer/SideDrawer.tsx
+++ src/components/side-drawer/SideDrawer.tsx
@@ -2,15 +2,40 @@ import React, { useContext } from 'react';
 import { ToolbarItems } from '@components/toolbar/ToolbarItems';
 import './side-drawer.scss';
 import { RootContext } from '@libs/context/root/root.context';
+import { graphql, useStaticQuery } from 'gatsby';
+import Img from 'gatsby-image';
+import { EmailIcon } from '../icons';
 
 export const SideDrawer = () => {
   const { getState } = useContext(RootContext);
   const show = getState(state => state.openSideDrawer);
 
+  const {
+    imageSharp: { fluid: imageFluid },
+  } = useStaticQuery(query);
+
   return (
-    <ToolbarItems
-      inDrawer
-      className={show ? 'side-drawer open' : 'side-drawer'}
-    />
+    <aside className={show ? 'side-drawer open' : 'open side-drawer'}>
+      <div className="photo-container">
+        <Img className="rounded-full image-shadow w-1/2" fluid={imageFluid} />
+        <div>
+          <p className="text-warning -mb-2 font-semibold">CRISTIAN SANTIZ</p>
+          <a href="mailto:crisantizan@gmail.com">
+            <span className="text-xs">crisantizan@gmail.com</span>
+          </a>
+        </div>
+      </div>
+      <ToolbarItems />
+    </aside>
   );
 };
+
+const query = graphql`
+  {
+    imageSharp(fluid: { originalName: { eq: "me.jpg" } }) {
+      fluid(maxWidth: 120) {
+        ...GatsbyImageSharpFluid
+      }
+    }
+  }
+`;
