2bfc706c7a8226d9923fef4db2ced3f726d43d1e
Cristian Santiz <crisantizan@gmail.com>
2020-03-08 21:17:14 -0500

    fix: add global provider

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,10 +2,10 @@ import React from 'react';
 import '@styles/tailwind.scss';
 import '@styles/index.scss';
 import { DefaultLayout } from '@layouts/DefaultLayout';
-import { RootProvider } from '@libs/context/root/root.context';
+import { GlobalProvider } from '@libs/context/global/context';
 
 export const wrapPageElement = ({ element, props }) => (
-  <RootProvider>
-    <DefaultLayout {...props}>{element}</DefaultLayout>
-  </RootProvider>
+  <GlobalProvider>
+    <DefaultLayout {...props}>{element}</DefaultLayout>>
+  </GlobalProvider>
 );
