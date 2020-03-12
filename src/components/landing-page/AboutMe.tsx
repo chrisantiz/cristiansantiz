4825b2bd416d1fff62d7bd5fd02347ebcc4980a6
Cristian Santiz <crisantizan@gmail.com>
2020-03-12 18:14:11 -0500

    style: add input animations

--- src/components/landing-page/AboutMe.tsx
+++ src/components/landing-page/AboutMe.tsx
@@ -20,11 +20,15 @@ export const AboutMe: React.FC<Props> = ({ id }) => {
     <PageContainer id={id}>
       <section className="py-3">
         {/* section 1 */}
-        <h2 className="text-2xl text-center text-warning font-semibold">
+        <h2
+          className="wow magictime boingInUp text-2xl text-center text-warning font-semibold"
+          data-wow-delay="100ms">
           Yo brevemente
         </h2>
 
-        <p className="text-justify">
+        <p
+          className="wow magictime boingInUp text-justify"
+          data-wow-delay="200ms">
           Me presento, mi nombre es Cristian Santiz, tengo {age} años y soy
           oriundo de Colombia, más específico de un pequeño pueblo ubicado al
           norte del país...{' '}
@@ -37,32 +41,42 @@ export const AboutMe: React.FC<Props> = ({ id }) => {
           , le dicen.
         </p>
 
-        <p className="text-justify mt-2">
+        <p
+          className="wow magictime boingInUp text-justify mt-2"
+          data-wow-delay="300ms">
           Soy un tipo común y corriente enamorado del desarrollo web. Tan
           autodidacta como curioso, siempre intento estar al tanto de las cosas
           que pasan alrededor del mundo, no solo tecnológicas; pues, tal cual
           citó Bacon: «el conocimiento es poder».
         </p>
 
-        <p className="text-justify mt-2">
+        <p
+          className="wow magictime boingInUp text-justify mt-2"
+          data-wow-delay="400ms">
           Filosofía, astronomía, ciencia y ortografía son otros temas que me
           atraen y de los cuales me declaro fánatico (no más que eso).
         </p>
 
-        <p className="text-justify mt-2">
+        <p
+          className="wow magictime boingInUp text-justify mt-2"
+          data-wow-delay="500ms">
           En resumen, soy del tipo de persona que le gusta saber el porqué de
           las cosas solo para dejar de nutrir su ignorancia.
         </p>
         {/* end section 1 */}
 
         {/* section 2 */}
-        <h2 className="mt-8 mb-4 text-2xl text-center text-warning font-semibold">
+        <h2
+          className="wow magictime boingInUp mt-8 mb-4 text-2xl text-center text-warning font-semibold"
+          data-wow-delay="600ms">
           Por qué deberías contratarme y/o trabajar conmigo
         </h2>
 
         {/* cards */}
         <section className="flex flex-wrap md:flex-no-wrap mb-4">
-          <div className="w-full md:mr-3">
+          <div
+            className="wow magictime spaceInLeft w-full md:mr-3"
+            data-wow-delay="700ms">
             <SimpleCard title="Competitividad">
               <CompetitivenessIcon className="w-32 h-32" />
               Lorem ipsum dolor sit amet, consectetur adipisicing elit.
@@ -71,7 +85,9 @@ export const AboutMe: React.FC<Props> = ({ id }) => {
             </SimpleCard>
           </div>
 
-          <div className="w-full md:mr-3 mt-3 md:mt-0">
+          <div
+            className="wow magictime spaceInDown w-full md:mr-3 mt-3 md:mt-0"
+            data-wow-delay="800ms">
             <SimpleCard title="Constancia">
               <ConstantIcon className="w-32 h-32" />
               Lorem ipsum dolor sit amet, consectetur adipisicing elit.
@@ -80,7 +96,9 @@ export const AboutMe: React.FC<Props> = ({ id }) => {
             </SimpleCard>
           </div>
 
-          <div className="w-full mt-3 md:mt-0">
+          <div
+            className="wow magictime spaceInRight w-full mt-3 md:mt-0"
+            data-wow-delay="900ms">
             <SimpleCard title="Productividad e ideas">
               <IdeasIcon className="w-32 h-32" />
               Lorem ipsum dolor sit amet, consectetur adipisicing elit.
--- src/components/landing-page/Skills.tsx
+++ src/components/landing-page/Skills.tsx
@@ -16,14 +16,14 @@ export const Skills: React.FC<Props> = ({ id }) => {
 
   return (
     <PageContainer className="pt-2 md:pt-6 min-h-screen" id={id}>
-      <h2 className="mb-2 md:mb-5 text-2xl text-center text-warning font-semibold">
+      <h2 className="wow magictime vanishIn mb-2 md:mb-5 text-2xl text-center text-warning font-semibold">
         {skills.linkLabel}
       </h2>
 
       <SkillsProgressBar />
       <p
         className="wow magictime vanishIn my-5 text-justify"
-        data-wow-delay="600ms">
+        data-wow-delay="500ms">
         {skills.text}
       </p>
     </PageContainer>
