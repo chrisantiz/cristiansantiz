a319dc8e09e410c9a80b8ef19bbd4f8f8086aee1
Cristian Santiz <crisantizan@gmail.com>
2020-03-12 16:06:02 -0500

    refactor: function classesByImageStatus

--- src/components/landing-page/Home.tsx
+++ src/components/landing-page/Home.tsx
@@ -9,7 +9,15 @@ import { useGlobalState } from '@/libs/hooks/use-global-state';
 
 interface Props {
   id: string;
-  // imageLoaded(): void;
+}
+
+interface ClassesByImageStatus {
+  /** print always */
+  always?: string;
+  /** before load */
+  from?: string;
+  /** after load */
+  to: string;
 }
 
 export const Home: React.FC<Props> = ({ id }) => {
@@ -28,6 +36,7 @@ export const Home: React.FC<Props> = ({ id }) => {
     state: { myData },
   } = useGlobalState();
 
+  /** execute when image has been loaded */
   function handleImageLoaded() {
     setImageLoaded(true);
 
@@ -39,93 +48,105 @@ export const Home: React.FC<Props> = ({ id }) => {
   function displaySpinner() {
     const element = (
       <div
-        className={`custom-spinner ${
-          !imageLoaded ? 'opacity-1' : 'opacity-0'
-        }`}>
+        className={classesByImageStatus({
+          from: 'opacity-1',
+          to: 'opacity-0',
+          always: 'custom-spinner',
+        })}>
         <div className="sp sp-wave" />
       </div>
     );
 
-    return showLoader ? element : null;
+    return showLoader && element;
+  }
+
+  /** return clasess according to image status */
+  function classesByImageStatus({
+    always = '',
+    from = '',
+    to,
+  }: ClassesByImageStatus) {
+    return (!imageLoaded ? from : to).concat(always && ` ${always}`);
   }
 
   return (
     <section className="landing-item home-section" id={id}>
       <PageContainer>
         <div
-          style={{
-            width: '100%',
-            height: '85vh',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            flexDirection: 'column',
-          }}>
+          style={{ height: '85vh' }}
+          className="w-full flex justify-center items-center flex-col">
           {displaySpinner()}
+
+          {/* image */}
           <div
-            className={
-              imageLoaded ? 'opacity-1 magictime spaceInRight' : 'opacity-0'
-            }>
+            className={classesByImageStatus({
+              from: 'opacity-0',
+              to: 'opacity-1 magictime spaceInRight',
+            })}>
             <Img
               onLoad={handleImageLoaded}
-              className={`w-40 sm:w-48 rounded-full image-shadow ${
-                imageLoaded ? 'magictime spaceInRight' : ''
-              }`}
+              className="w-40 sm:w-48 rounded-full image-shadow"
               fluid={image.imageSharp.fluid}
             />
           </div>
 
-          <div
-            className={`flex justify-center items-center flex-col ${
-              imageLoaded ? 'opacity-1' : 'opacity-0'
-            }`}>
-            {/* name */}
-            <p
-              className="wow magictime swap text-warning font-bold text-2xl uppercase"
-              data-wow-delay="400ms">
-              {myData.name.short}
-            </p>
-            {/* role */}
-            <div
-              className="wow magictime spaceInRight flex items-center text-xl web-developer font-semibold -mt-2"
-              data-wow-delay="500ms">
-              <span className="text-warning text-2xl font-semibold mr-1">
-                &#60;
+          {/* render only when image has been loaded */}
+          {imageLoaded && (
+            <div className="flex justify-center items-center flex-col">
+              {/* name */}
+              <p
+                className="wow magictime swap text-warning font-bold text-2xl uppercase"
+                data-wow-delay="400ms">
+                {myData.name.short}
+              </p>
+
+              {/* role */}
+              <div
+                className="wow magictime spaceInRight flex items-center text-xl web-developer font-semibold -mt-2"
+                data-wow-delay="500ms">
+                <span className="text-warning text-2xl font-semibold mr-1">
+                  &#60;
+                </span>
+                WebDeveloper
+                <span className="text-warning text-2xl font-semibold ml-1">
+                  /&#62;
+                </span>
+              </div>
+
+              {/* message */}
+              <span
+                className="wow magictime swap sm:w-1/2 text-center mt-1"
+                data-wow-delay="600ms">
+                {siteDescription}
               </span>
-              WebDeveloper
-              <span className="text-warning text-2xl font-semibold ml-1">
-                /&#62;
-              </span>
+
+              {/* button */}
+              <Button
+                to="sobre-mi"
+                outlined
+                size="sm"
+                className="wow magictime spaceInRight my-3"
+                data-wow-delay="700ms"
+                data-wow-offset="-56">
+                {home.labels.buttonKnowMore}
+              </Button>
+
+              {/* social media icons */}
+              <SocialMediaIcons
+                className="wow magictime swap mt-3"
+                data-wow-delay="750ms"
+                data-wow-offset="-56"
+              />
+
+              {/* current location */}
+              <small
+                className="wow magictime swap"
+                data-wow-delay="800ms"
+                data-wow-offset="-56">
+                {myData.currentLocation}
+              </small>
             </div>
-            {/* message */}
-            <span
-              className="wow magictime swap sm:w-1/2 text-center mt-1"
-              data-wow-delay="600ms">
-              {siteDescription}
-            </span>
-            {/* button */}
-            <Button
-              to="sobre-mi"
-              outlined
-              size="sm"
-              className="wow magictime spaceInRight my-3"
-              data-wow-delay="700ms"
-              data-wow-offset="-56">
-              {home.labels.buttonKnowMore}
-            </Button>
-            {/* social media icons */}
-            <SocialMediaIcons
-              className="wow magictime swap mt-3"
-              data-wow-delay="750ms"
-              data-wow-offset="-56"
-            />
-            <small
-              className="wow magictime swap"
-              data-wow-delay="800ms"
-              data-wow-offset="-56">
-              {myData.currentLocation}
-            </small>
-          </div>
+          )}
         </div>
       </PageContainer>
     </section>
