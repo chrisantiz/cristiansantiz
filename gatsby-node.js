af99ea5377c08a008436624fa2e1163a5268ffe4
Cristian Santiz <crisantizan@gmail.com>
2020-05-30 16:31:02 -0500

    refactor: remove on create node hook

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,5 +2,3 @@ require('source-map-support').install();
 require('ts-node').register();
 
 exports.onCreatePage = require('./src/libs/node/on-create-page').onCreatePage;
-
-exports.onCreateNode = require('./src/libs/node/on-create-node').onCreateNode;
