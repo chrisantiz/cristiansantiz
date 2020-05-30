05b0cdd5b80b79b934bdab558927b89e2ba17c10
Cristian Santiz <crisantizan@gmail.com>
2020-05-30 17:50:48 -0500

    refactor: configure gatsby-plugin-robots-txt

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,46 @@
 const path = require('path');
+const {
+  NODE_ENV,
+  URL: NETLIFY_SITE_URL = 'https://cristiansantiz.netlify.app',
+  DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
+  CONTEXT: NETLIFY_ENV = NODE_ENV,
+} = process.env;
+
+const isNetlifyProduction = NETLIFY_ENV === 'production';
+const siteUrl = isNetlifyProduction ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL;
 
 module.exports = {
   siteMetadata: {
     title: `Cristian Santiz <web developer>`,
     description: `Desarrollador web freelance. Amante del autoaprendizaje, el conocimiento libre y de Javascript`,
     author: `Cristian Santiz <crisantizan@gmail.com>`,
+    siteUrl,
   },
   plugins: [
     `gatsby-plugin-typescript`,
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-robots-txt`,
     `gatsby-plugin-sitemap`,
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        resolveEnv: () => NETLIFY_ENV,
+        env: {
+          production: {
+            policy: [{ userAgent: '*' }],
+          },
+          'branch-deploy': {
+            policy: [{ userAgent: '*', disallow: ['/'] }],
+            sitemap: null,
+            host: null,
+          },
+          'deploy-preview': {
+            policy: [{ userAgent: '*', disallow: ['/'] }],
+            sitemap: null,
+            host: null,
+          },
+        },
+      },
+    },
     {
       resolve: `gatsby-plugin-alias-imports`,
       options: {
