56bbba9d4db958f4a30294a08ca2bb58f7197a65
Cristian Santiz <crisantizan@gmail.com>
2020-06-13 23:09:03 -0500

    feat: add endpoints

--- server/routes.js
+++ server/routes.js
@@ -1,12 +1,27 @@
+const links = require('./links.json');
 /**
- *
  * @param {import("fastify").FastifyInstance} fastify
- * @param {*} opts
- * @param {*} done
  */
 module.exports = function(fastify, opts, done) {
+  // display availables routes
   fastify.get('/', (req, reply) => {
-    reply.send({ message: 'With Fastify' });
+    reply.send({
+      routes: {
+        '/cv': 'Currículum personal',
+        '/kirshas': 'video demo del proyecto Kirshas',
+      },
+    });
   });
+
+  // get cv
+  fastify.get('/cv', (req, reply) => {
+    reply.redirect(links.cv);
+  });
+
+  // get kirshas demo
+  fastify.get('/kirshas', (req, reply) => {
+    reply.redirect(links.kirshasDemo);
+  });
+
   done();
 };
