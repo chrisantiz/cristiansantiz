eab13895546e052a8bcc3ef1303b431cf590772d
Cristian Santiz <crisantizan@gmail.com>
2020-06-13 22:31:04 -0500

    feat: netlify redirects

--- functions/redirect-links/redirect-links.js
+++ functions/redirect-links/redirect-links.js
@@ -1,11 +1,11 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 exports.handler = async (event, context) => {
   try {
-    const subject = event.queryStringParameters.name || 'World';
+    console.log(context);
     return {
       statusCode: 200,
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ message: `Hello ${subject}` }),
+      body: JSON.stringify(require('../links.json')),
     };
   } catch (err) {
     return { statusCode: 500, body: err.toString() };
