9697c0f39686cf84f827c876222633b39a9e4bc2
Cristian Santiz <crisantizan@gmail.com>
2020-06-13 23:21:04 -0500

    chore: remove comment

--- src/components/side-drawer/SideDrawer.tsx
+++ src/components/side-drawer/SideDrawer.tsx
@@ -8,7 +8,6 @@ import moment from 'moment';
 import { useSelector } from '@/libs/context/global/context';
 
 const SideDrawer = () => {
-  console.log('RENDER SIDE-DRAWER')
   const { openSideDrawer: show, myData } = useSelector(s => s);
 
   const {
