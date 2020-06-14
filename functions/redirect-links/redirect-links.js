630e3f8c7e1841993499bc691c2c2dca760e7912
Cristian Santiz <crisantizan@gmail.com>
2020-06-13 21:55:26 -0500

    feat: add netlify functions

---
+++ functions/redirect-links/redirect-links.js
@@ -0,0 +1,13 @@
+// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+exports.handler = async (event, context) => {
+  try {
+    const subject = event.queryStringParameters.name || 'World';
+    return {
+      statusCode: 200,
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ message: `Hello ${subject}` }),
+    };
+  } catch (err) {
+    return { statusCode: 500, body: err.toString() };
+  }
+};
